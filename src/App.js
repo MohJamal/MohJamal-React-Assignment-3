395ce071715cec30369a64a57640a342135b97ab
MohJamal <mohjamalalkhatib96@gmail.com>
2021-09-16 17:16:22 +0300

    user part is done

--- src/App.js
+++ src/App.js
@@ -8,12 +8,32 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 function App() {
   const [loading, setLoading] = useState(true);
   const [articlesData, setArticlesData] = useState([]);
+  const [usersData, setUsersData] = useState([]);
+
+  let axiosConfig = {
+    headers: {
+      "app-id": "613e64e2748b558d779ce190",
+    },
+  };
+
+  const fetchData = () => {
+    const articlesAPI = "https://jsonplaceholder.typicode.com/posts";
+    const usersAPI = "https://dummyapi.io/data/v1/user?limit=50";
+
+    const getArticles = axios.get(articlesAPI);
+    const getUsers = axios.get(usersAPI, axiosConfig);
+
+    axios.all([getArticles, getUsers]).then(
+      axios.spread((...allData) => {
+        setArticlesData(allData[0].data);
+        setUsersData(allData[1].data.data);
+        setLoading(false);
+      })
+    );
+  };
 
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
-      setArticlesData(res.data);
-      setLoading(false);
-    });
+    fetchData();
   }, []);
 
   return (
@@ -23,7 +43,7 @@ function App() {
           <h2>Articles</h2>
           <Articles articlesData={articlesData} />
           <h2>Users</h2>
-          <Users />
+          <Users usersData={usersData} />
         </>
       ) : (
         <CircularProgress style={{ width: 70, height: 70 }} />
--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import "./Users.css";
 
-const Users = () => {
-  return <div></div>;
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+});
+
+const Users = (props) => {
+  const classes = useStyles();
+
+  return (
+    <div className="Users">
+      {props.usersData.map((user) => {
+        let userTitle =
+          user.title.charAt(0).toUpperCase() +
+          user.title.slice(1, user.title.length) +
+          ".";
+
+        return (
+          <Card className={classes.root} variant="outlined" key={user.id}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                {userTitle + " " + user.firstName + " " + user.lastName}
+              </Typography>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Users;
